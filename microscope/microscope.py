99211f663b050a4ba0044ec4719e98909519aba8
Maciej Kwiek <maciej.iai@gmail.com>
2018-02-05 16:59:31 +0100

    MonitorRunner and monitor module added
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

--- microscope/microscope.py
+++ microscope/microscope.py
@@ -1,135 +1,18 @@
 import time
-import json
-import signal
 import argparse
-import sys
-from multiprocessing import Process, Queue
+from multiprocessing import Queue
 import queue as queuemodule
 import threading
-from typing import List, Set
+from typing import List
 
 from kubernetes import config
 from kubernetes.client import Configuration
 from kubernetes.client.apis import core_v1_api
-from kubernetes.client.rest import ApiException
-from kubernetes.stream import stream
 
 import urwid
 import urwid.raw_display
 
-
-# we are ignoring sigint in monitor processes as they are closed via queue
-def sigint_in_monitor(signum, frame):
-    pass
-
-
-class Monitor:
-    def __init__(self,
-                 pod_name: str,
-                 namespace: str,
-                 queue: Queue,
-                 close_queue: Queue,
-                 api: core_v1_api.CoreV1Api,
-                 endpoints: Set[int],
-                 verbose: bool):
-        self.pod_name = pod_name
-        self.namespace = namespace
-        self.queue = queue
-        self.close_queue = close_queue
-        self.api = api
-        self.endpoints = endpoints
-        self.verbose = verbose
-        self.process = Process(target=connect_monitor, args=(self,))
-        self.output = pod_name + "\n"
-        self.output_lock = threading.Semaphore()
-
-
-def connect_monitor(m: Monitor):
-    try:
-        resp = m.api.read_namespaced_pod(name=m.pod_name,
-                                         namespace=m.namespace)
-    except ApiException as e:
-        if e.status != 404:
-            print('Unknown error: %s' % e)
-            exit(1)
-
-    # calling exec and wait for response.
-    exec_command = [
-        'cilium',
-        'monitor']
-
-    if m.verbose:
-        exec_command.append('-v')
-
-    if m.endpoints:
-        for e in m.endpoints:
-            exec_command.append('--related-to')
-            exec_command.append(str(e))
-
-    resp = stream(m.api.connect_get_namespaced_pod_exec, m.pod_name,
-                  m.namespace,
-                  command=exec_command,
-                  stderr=True, stdin=True,
-                  stdout=True, tty=True,
-                  _preload_content=False)
-
-    signal.signal(signal.SIGINT, sigint_in_monitor)
-
-    while resp.is_open():
-        resp.update(timeout=1)
-        if not m.close_queue.empty():
-            print("Closing monitor")
-            resp.write_stdin('\x03')
-            break
-        if resp.peek_stdout():
-            m.queue.put({'name': m.pod_name,  'output': resp.read_stdout()})
-        if resp.peek_stderr():
-            m.queue.put({'name': m.pod_name,  'output': resp.read_stderr()})
-
-    resp.close()
-
-    m.close_queue.cancel_join_thread()
-    m.queue.cancel_join_thread()
-
-
-def run_monitors(verbose: bool, selectors: List[str],
-                 pod_names: List[str], endpoints: List[int], queue: Queue,
-                 close_queue: Queue) -> List[Monitor]:
-
-    try:
-        config.load_kube_config()
-    except FileNotFoundError:
-        config.load_incluster_config()
-
-    c = Configuration()
-    c.assert_hostname = False
-    Configuration.set_default(c)
-    # TODO: move these two to new Monitors class
-    api = core_v1_api.CoreV1Api()
-    namespace = 'kube-system'
-
-    try:
-        pods = api.list_namespaced_pod(namespace,
-                                       label_selector='k8s-app=cilium')
-    except ApiException as e:
-        print('could not list Cilium pods: %s\n' % e)
-        sys.exit(1)
-
-    names = [pod.metadata.name for pod in pods.items]
-
-    ids = retrieve_endpoint_ids(api, selectors, pod_names, names)
-    ids.update(endpoints)
-
-    print(ids)
-
-    monitors = [
-        Monitor(name, namespace, queue, close_queue, api, ids, verbose)
-        for name in names]
-
-    for m in monitors:
-        m.process.start()
-
-    return monitors
+from monitor import Monitor, MonitorRunner
 
 
 def close_monitors(close_queue: Queue, monitors: List[Monitor]):
@@ -139,48 +22,9 @@ def close_monitors(close_queue: Queue, monitors: List[Monitor]):
         m.process.join()
 
 
-def retrieve_endpoint_ids(api: core_v1_api.CoreV1Api, selectors: List[str],
-                          pod_names: List[str], node_names: List[str]
-                          ) -> Set[int]:
-    ids = set()
-    for node in node_names:
-        exec_command = ['cilium', 'endpoint', 'list', '-o', 'json']
-        resp = stream(api.connect_get_namespaced_pod_exec, node, 'kube-system',
-                      command=exec_command,
-                      stderr=False, stdin=False,
-                      stdout=True, tty=False, _preload_content=False,
-                      _return_http_data_only=True)
-        output = ""
-
-        # _preload_content causes json to be malformed,
-        # so we need to load raw data from websocket
-        while resp.is_open():
-            resp.update(timeout=1)
-            if resp.peek_stdout():
-                output += resp.read_stdout()
-            try:
-                data = json.loads(output)
-                resp.close()
-            except ValueError:
-                continue
-
-        nameMatch = {endpoint['id'] for endpoint in data
-                     if endpoint['pod-name'] in pod_names}
-
-        labelsMatch = {endpoint['id'] for endpoint in data
-                       if any(
-                           [any([selector in label for selector in selectors])
-                            for label
-                            in endpoint['labels']['orchestration-identity']]
-                       )}
-        ids.update(nameMatch, labelsMatch)
-
-    return ids
-
-
-def ui(monitors: List[Monitor]):
+def ui(runner: MonitorRunner):
     monitor_columns = {m.pod_name: (urwid.Text(m.output), m)
-                       for m in monitors}
+                       for m in runner.monitors}
 
     text_header = (u"Cilium Monitor Sink."
                    u"UP / DOWN / PAGE UP / PAGE DOWN scroll. F8 exits. "
@@ -213,7 +57,7 @@ def ui(monitors: List[Monitor]):
     def dump_data():
         timestamp = time.time()
         outputs = {}
-        for m in monitors:
+        for m in runner.monitors:
             if m.output_lock.acquire():
                 outputs[m.pod_name] = m.output
                 m.output_lock.release()
@@ -231,11 +75,7 @@ def ui(monitors: List[Monitor]):
     mainloop = urwid.MainLoop(frame, palette, screen,
                               unhandled_input=unhandled)
 
-    def wait_for_values(monitor_columns):
-        m = next(iter(monitor_columns.values()))
-        queue = m[1].queue
-        close_queue = m[1].close_queue
-
+    def wait_for_values(monitor_columns, queue, close_queue):
         while(close_queue.empty()):
             try:
                 output = queue.get(True, 5)
@@ -251,7 +91,9 @@ def ui(monitors: List[Monitor]):
             column.set_text(monitor.output)
             mainloop.draw_screen()
     update_thread = threading.Thread(target=wait_for_values,
-                                     args=(monitor_columns,))
+                                     args=(monitor_columns,
+                                           runner.data_queue,
+                                           runner.close_queue))
     update_thread.start()
 
     mainloop.run()
@@ -274,11 +116,23 @@ def main():
 
     q = Queue()
     close_queue = Queue()
-    monitors = run_monitors(args.verbose, args.selector,
-                            args.pod, args.endpoint, q, close_queue)
 
-    ui(monitors)
-    close_monitors(close_queue, monitors)
+    try:
+        config.load_kube_config()
+    except FileNotFoundError:
+        config.load_incluster_config()
+
+    c = Configuration()
+    c.assert_hostname = False
+    Configuration.set_default(c)
+    api = core_v1_api.CoreV1Api()
+    runner = MonitorRunner('kube-system', api, q, close_queue)
+
+    runner.run(args.verbose, args.selector,
+               args.pod, args.endpoint)
+
+    ui(runner)
+    close_monitors(close_queue, runner.monitors)
 
 
 if __name__ == '__main__':
---
+++ microscope/monitor.py
@@ -0,0 +1,166 @@
+from typing import Set, List
+import json
+import sys
+import signal
+import threading
+from multiprocessing import Process, Queue
+
+from kubernetes.client.apis import core_v1_api
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
+
+
+# we are ignoring sigint in monitor processes as they are closed via queue
+def sigint_in_monitor(signum, frame):
+    pass
+
+
+class Monitor:
+    def __init__(self,
+                 pod_name: str,
+                 namespace: str,
+                 queue: Queue,
+                 close_queue: Queue,
+                 api: core_v1_api.CoreV1Api,
+                 endpoints: Set[int],
+                 verbose: bool):
+        self.pod_name = pod_name
+        self.namespace = namespace
+        self.queue = queue
+        self.close_queue = close_queue
+        self.api = api
+        self.endpoints = endpoints
+        self.verbose = verbose
+        self.process = Process(target=self.connect)
+        self.output = pod_name + "\n"
+        self.output_lock = threading.Semaphore()
+
+    def connect(self):
+        try:
+            resp = self.api.read_namespaced_pod(name=self.pod_name,
+                                                namespace=self.namespace)
+        except ApiException as e:
+            if e.status != 404:
+                print('Unknown error: %s' % e)
+                exit(1)
+
+        # calling exec and wait for response.
+        exec_command = [
+            'cilium',
+            'monitor']
+
+        if self.verbose:
+            exec_command.append('-v')
+
+        if self.endpoints:
+            for e in self.endpoints:
+                exec_command.append('--related-to')
+                exec_command.append(str(e))
+
+        resp = stream(self.api.connect_get_namespaced_pod_exec, self.pod_name,
+                      self.namespace,
+                      command=exec_command,
+                      stderr=True, stdin=True,
+                      stdout=True, tty=True,
+                      _preload_content=False)
+
+        signal.signal(signal.SIGINT, sigint_in_monitor)
+
+        while resp.is_open():
+            resp.update(timeout=1)
+            if not self.close_queue.empty():
+                print("Closing monitor")
+                resp.write_stdin('\x03')
+                break
+            if resp.peek_stdout():
+                self.queue.put({
+                    'name': self.pod_name,
+                    'output': resp.read_stdout()})
+            if resp.peek_stderr():
+                self.queue.put({
+                    'name': self.pod_name,
+                    'output': resp.read_stderr()})
+
+        resp.close()
+
+        self.close_queue.cancel_join_thread()
+        self.queue.cancel_join_thread()
+
+
+class MonitorRunner:
+    def __init__(self, namespace, api, data_queue: Queue,
+                 close_queue: Queue):
+        self.namespace = namespace
+        self.api = api
+        self.monitors = []
+        self.data_queue = data_queue
+        self.close_queue = close_queue
+
+    def run(self, verbose: bool, selectors: List[str],
+            pod_names: List[str], endpoints: List[int]):
+
+        # TODO: move these two to new Monitors class
+        api = core_v1_api.CoreV1Api()
+        namespace = 'kube-system'
+
+        try:
+            pods = api.list_namespaced_pod(namespace,
+                                           label_selector='k8s-app=cilium')
+        except ApiException as e:
+            print('could not list Cilium pods: %s\n' % e)
+            sys.exit(1)
+
+        names = [pod.metadata.name for pod in pods.items]
+
+        ids = self.retrieve_endpoint_ids(selectors, pod_names, names)
+        ids.update(endpoints)
+
+        self.monitors = [
+            Monitor(name, namespace, self.data_queue, self.close_queue, api,
+                    ids, verbose)
+            for name in names]
+
+        for m in self.monitors:
+            m.process.start()
+
+    def retrieve_endpoint_ids(self, selectors: List[str],
+                              pod_names: List[str],
+                              node_names: List[str]
+                              ) -> Set[int]:
+        ids = set()
+        for node in node_names:
+            exec_command = ['cilium', 'endpoint', 'list', '-o', 'json']
+            resp = stream(self.api.connect_get_namespaced_pod_exec, node,
+                          self.namespace,
+                          command=exec_command,
+                          stderr=False, stdin=False,
+                          stdout=True, tty=False, _preload_content=False,
+                          _return_http_data_only=True)
+            output = ""
+
+            # _preload_content causes json to be malformed,
+            # so we need to load raw data from websocket
+            while resp.is_open():
+                resp.update(timeout=1)
+                if resp.peek_stdout():
+                    output += resp.read_stdout()
+                try:
+                    data = json.loads(output)
+                    resp.close()
+                except ValueError:
+                    continue
+
+            nameMatch = {endpoint['id'] for endpoint in data
+                         if endpoint['pod-name'] in pod_names}
+
+            labelsMatch = {endpoint['id'] for endpoint in data
+                           if any([
+                               any(
+                                   [selector in label
+                                    for selector in selectors])
+                               for label
+                               in endpoint['labels']['orchestration-identity']
+                           ])}
+            ids.update(nameMatch, labelsMatch)
+
+        return ids
