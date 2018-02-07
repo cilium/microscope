c8acc125a8cbecf3e29b0b50a1a50722d667880a
Maciej Kwiek <maciej.iai@gmail.com>
2018-02-07 13:53:21 +0100

    Add MonitorArgs class, move cmd building to runner
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

--- microscope/microscope.py
+++ microscope/microscope.py
@@ -4,13 +4,16 @@ from kubernetes import config
 from kubernetes.client import Configuration
 from kubernetes.client.apis import core_v1_api
 
-from monitor import MonitorRunner
+from monitor import MonitorRunner, MonitorArgs
 from ui import ui
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', type=bool, default=False)
+    parser.add_argument('--node', action='append', default=[],
+                        help='Cilium pod names. Can specify multiple.')
+
     parser.add_argument('--selector', action='append', default=[],
                         help='k8s equality label selectors for pods which '
                         'monitor should listen to. each selector will '
@@ -22,8 +25,39 @@ def main():
                         'Can specify multiple.')
     parser.add_argument('--endpoint', action='append', type=int, default=[],
                         help='Cilium endpoint ids. Can specify multiple.')
-    parser.add_argument('--node', action='append', default=[],
-                        help='Cilium pod names. Can specify multiple.')
+
+    parser.add_argument('--to-selector', action='append', default=[],
+                        help='k8s equality label selectors for pods which '
+                        'monitor should listen to. each selector will '
+                        'retrieve its own set of pods. '
+                        'Matches events that go to selected pods. '
+                        'Format is "label-name=label-value" '
+                        'Can specify multiple.')
+    parser.add_argument('--to-pod', action='append', default=[],
+                        help='pod names in form of "namespace:pod-name" '
+                        'Matches events that go to specified pods. '
+                        'Can specify multiple.')
+    parser.add_argument('--to-endpoint', action='append', type=int, default=[],
+                        help='Cilium endpoint ids. '
+                        'Matches events that go to specified endpoints. '
+                        'Can specify multiple.')
+
+    parser.add_argument('--from-selector', action='append', default=[],
+                        help='k8s equality label selectors for pods which '
+                        'monitor should listen to. each selector will '
+                        'retrieve its own set of pods. '
+                        'Matches events that come from selected pods. '
+                        'Format is "label-name=label-value" '
+                        'Can specify multiple.')
+    parser.add_argument('--from-pod', action='append', default=[],
+                        help='pod names in form of "namespace:pod-name" '
+                        'Matches events that come from specified pods. '
+                        'Can specify multiple.')
+    parser.add_argument('--from-endpoint', action='append', type=int,
+                        default=[],
+                        help='Cilium endpoint ids. '
+                        'Matches events that come from specified endpoints. '
+                        'Can specify multiple.')
 
     args = parser.parse_args()
 
@@ -38,9 +72,13 @@ def main():
     api = core_v1_api.CoreV1Api()
     runner = MonitorRunner('kube-system', api)
 
+    monitor_args = MonitorArgs(args.verbose, args.selector, args.pod,
+                               args.endpoint, args.to_selector, args.to_pod,
+                               args.to_endpoint, args.from_selector,
+                               args.from_pod, args.from_endpoint)
+
     try:
-        runner.run(args.verbose, args.selector,
-                   args.pod, args.endpoint, args.node)
+        runner.run(monitor_args, args.node)
     except ValueError as e:
         print(e)
         runner.finish()
--- microscope/monitor.py
+++ microscope/monitor.py
@@ -4,10 +4,35 @@ import sys
 import signal
 import threading
 from multiprocessing import Process, Queue
+import queue as queuemodule
 
 from kubernetes.client.apis import core_v1_api
 from kubernetes.client.rest import ApiException
 from kubernetes.stream import stream
+
+
+class MonitorArgs:
+    def __init__(self,
+                 verbose: bool,
+                 related_selectors: List[str],
+                 related_pods: List[str],
+                 related_endpoints: List[int],
+                 to_selectors: List[str],
+                 to_pods: List[str],
+                 to_endpoints: List[int],
+                 from_selectors: List[str],
+                 from_pods: List[str],
+                 from_endpoints: List[int]):
+        self.verbose = verbose
+        self.related_selectors = related_selectors
+        self.related_pods = related_pods
+        self.related_endpoints = related_endpoints
+        self.to_selectors = to_selectors
+        self.to_pods = to_pods
+        self.to_endpoints = to_endpoints
+        self.from_selectors = from_selectors
+        self.from_pods = from_pods
+        self.from_endpoints = from_endpoints
 
 
 # we are ignoring sigint in monitor processes as they are closed via queue
@@ -22,15 +47,15 @@ class Monitor:
                  queue: Queue,
                  close_queue: Queue,
                  api: core_v1_api.CoreV1Api,
-                 endpoints: Set[int],
-                 verbose: bool):
+                 cmd: List[str],
+                 ):
         self.pod_name = pod_name
         self.namespace = namespace
         self.queue = queue
         self.close_queue = close_queue
         self.api = api
-        self.endpoints = endpoints
-        self.verbose = verbose
+        self.cmd = cmd
+
         self.process = Process(target=self.connect)
         self.output = pod_name + "\n"
         self.output_lock = threading.Semaphore()
@@ -45,21 +70,10 @@ class Monitor:
                 exit(1)
 
         # calling exec and wait for response.
-        exec_command = [
-            'cilium',
-            'monitor']
-
-        if self.verbose:
-            exec_command.append('-v')
-
-        if self.endpoints:
-            for e in self.endpoints:
-                exec_command.append('--related-to')
-                exec_command.append(str(e))
 
         resp = stream(self.api.connect_get_namespaced_pod_exec, self.pod_name,
                       self.namespace,
-                      command=exec_command,
+                      command=self.cmd,
                       stderr=True, stdin=True,
                       stdout=True, tty=True,
                       _preload_content=False)
@@ -95,14 +109,12 @@ class MonitorRunner:
         self.data_queue = Queue()
         self.close_queue = Queue()
 
-    def run(self, verbose: bool, selectors: List[str],
-            pod_names: List[str], endpoints: List[int], nodes: List[str]):
+    def run(self, monitor_args: MonitorArgs, nodes: List[str]):
 
         api = core_v1_api.CoreV1Api()
-        namespace = 'kube-system'
 
         try:
-            pods = api.list_namespaced_pod(namespace,
+            pods = api.list_namespaced_pod(self.namespace,
                                            label_selector='k8s-app=cilium')
         except ApiException as e:
             print('could not list Cilium pods: %s\n' % e)
@@ -118,16 +130,35 @@ class MonitorRunner:
             raise ValueError('No Cilium nodes in cluster match provided names'
                              ', or Cilium is not deployed')
 
-        ids = self.retrieve_endpoint_ids(selectors, pod_names, names)
-        ids.update(endpoints)
+        cmd = self.get_monitor_command(monitor_args, names)
 
         self.monitors = [
-            Monitor(name, namespace, self.data_queue, self.close_queue, api,
-                    ids, verbose)
+            Monitor(name, self.namespace, self.data_queue, self.close_queue,
+                    api, cmd)
             for name in names]
 
         for m in self.monitors:
             m.process.start()
+
+    def get_monitor_command(self, args: MonitorArgs, names: List[str]
+                            ) -> List[str]:
+        ids = self.retrieve_endpoint_ids(args.related_selectors,
+                                         args.related_pods, names)
+        ids.update(args.related_endpoints)
+
+        exec_command = [
+            'cilium',
+            'monitor']
+
+        if args.verbose:
+            exec_command.append('-v')
+
+        if ids:
+            for e in ids:
+                exec_command.append('--related-to')
+                exec_command.append(str(e))
+
+        return exec_command
 
     def retrieve_endpoint_ids(self, selectors: List[str],
                               pod_names: List[str],
@@ -139,7 +170,12 @@ class MonitorRunner:
         for p in getters:
             p.start()
 
-        id_sets = [self.data_queue.get() for _ in getters]
+        try:
+            id_sets = [self.data_queue.get(timeout=10) for _ in getters]
+        except queuemodule.Empty as e:
+            for p in getters:
+                p.terminate()
+            raise e
 
         for p in getters:
             p.join()
