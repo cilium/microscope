648ca4e3b9e36bac6f0377921162c54d6da06a9b
Maciej Kwiek <maciej.iai@gmail.com>
2018-02-13 12:29:31 +0100

    Add setup.py
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import io
+import sys
+import uuid
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+version = "0.1"
+
+
+with io.open('README.rst', 'r', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+if sys.argv[-1] == 'readme':
+    print(readme)
+    sys.exit()
+
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+requirements = [str(req.req) for req in install_reqs]
+
+setup(
+    name='microscope',
+    version=version,
+    description=('An urwid-based interface for watching '
+                 '`cilium monitor` events across your cluster'),
+    long_description=readme,
+    author='Maciej Kwiek',
+    author_email='maciej@covalent.io',
+    url='https://github.com/cilium/microscope',
+    packages=[
+        'microscope',
+    ],
+    entry_points={
+        'console_scripts': [
+            'micro = microscope.microscope:main',
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license='Apache 2.0',
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Networking :: Monitoring',
+    ],
+    keywords='microscope, cilium, monitor, k8s, kubernetes',
+)
