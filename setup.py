83b445920c321e28732e2c3d4f290494c8aee93b
Ray Bejjani <ray@covalent.io>
2018-05-18 12:35:09 +0200

    Support pip 10 pip._internal.req import path
    pip 10 moved pip.req to pip._internal.req. This is used during installs
    and it's better to support old and new versions.
    
    Signed-off-by: Ray Bejjani <ray@covalent.io>

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ import sys
 import uuid
 
 from setuptools import setup
-from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 version = "1.0.0"
 
