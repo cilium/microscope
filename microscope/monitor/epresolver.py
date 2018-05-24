c224cd2be9ca84a7328bd59fa14f56fd62c95463
Maciej Kwiek <maciej.iai@gmail.com>
2018-05-24 16:59:54 +0200

    Rebase fixes for EndpointResolver
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

--- microscope/monitor/epresolver.py
+++ microscope/monitor/epresolver.py
@@ -30,26 +30,26 @@ class EndpointResolver:
         self.epid_resolutions_regex = None
 
         if resolve_ips:  # if false, use the empty dict above that does no work
-            for epdata in endpoint_data:
-                for ep in epdata:
-                    podname = ep['status']['external-identifiers']['pod-name']
-                    for ip in ep['status']['networking']['addressing']:
-                        self.ip_resolutions[ip['ipv4']] = podname
-                        self.ip_resolutions[ip['ipv6']] = podname
+            for ep in endpoint_data:
+                podname = ep['status']['external-identifiers']['pod-name']
+                for ip in ep['status']['networking']['addressing']:
+                    self.ip_resolutions[ip['ipv4']] = podname
+                    self.ip_resolutions[ip['ipv6']] = podname
             self.ip_resolutions_regex = re.compile(
                 "(%s)" % "|".join(map(re.escape, self.ip_resolutions.keys())))
 
         if resolve_ids:  # if false, use the empty dict above that does no work
-            for epdata in endpoint_data:
-                for ep in epdata:
-                    podname = ep['status']['external-identifiers']['pod-name']
-                    # the str(ep['id']) below is needed because the ID is an
-                    # int in json
-                    self.epid_resolutions["endpoint "+str(ep['id'])] = (
-                        "endpoint " + podname)
+            for ep in endpoint_data:
+                podname = ep['status']['external-identifiers']['pod-name']
+                # the str(ep['id']) below is needed because the ID is an
+                # int in json
+                self.epid_resolutions["endpoint "+str(ep['id'])] = (
+                    "endpoint " + podname)
             self.epid_resolutions_regex = re.compile(
                 "(%s)" % "|".join(map(re.escape,
                                       self.epid_resolutions.keys())))
+        print(self.ip_resolutions)
+        print(self.epid_resolutions)
 
     def resolve_to_podnames(self, line):
         """replace fields in line with the podname, if configured"""
@@ -58,3 +58,13 @@ class EndpointResolver:
         line = substitute(self.epid_resolutions_regex, self.epid_resolutions,
                           line)
         return line
+
+    def ip_to_podname(self, ip) -> str:
+        if ip in self.ip_resolutions:
+            return self.ip_resolutions[ip]
+        return ip
+
+    def eid_to_podname(self, eid) -> str:
+        if eid in self.epid_resolutions:
+            return self.epid_resolutions[eid]
+        return eid
